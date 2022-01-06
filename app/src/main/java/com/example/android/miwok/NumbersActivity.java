5e639854280e18ba5919c64935d8fdc3fac9d845
Oleksandr Bondarevskyi <epickonst@gmail.com>
2022-01-06 14:26:15 +0100

    lesson 2.8 added ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -19,6 +19,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -26,21 +28,21 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] words = new String[10];
+        ArrayList<String> words = new ArrayList<String>();
 
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
         for (int i = 0; i < 10; i++) {
-            Log.v("NumbersActivity", "Word at index " + i + ": " + words[i] );
+            Log.v("NumbersActivity", "Word at index " + i + ": " + words.get(i) );
         }
     }
 }
