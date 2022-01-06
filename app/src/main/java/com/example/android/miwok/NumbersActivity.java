7d488bd7ddc296e39b87c6af8a485d8c708c4999
Oleksandr Bondarevskyi <epickonst@gmail.com>
2022-01-06 21:33:48 +0100

    lesson 2.11

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,6 +18,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -41,8 +43,19 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        for (int i = 0; i < 10; i++) {
-            Log.v("NumbersActivity", "Word at index " + i + ": " + words.get(i) );
+        LinearLayout numbersLL = (LinearLayout) findViewById(R.id.numbersLL);
+
+        int index = 0;
+
+        while (index < words.size()) {
+
+            TextView wordView = new TextView(this);
+
+            wordView.setText(words.get(index));
+
+            numbersLL.addView(wordView);
+
+            index++;
         }
     }
 }
