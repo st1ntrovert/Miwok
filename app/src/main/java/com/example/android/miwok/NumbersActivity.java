62bdca983805a7100a84faa486dfc58d2df92cdb
Oleksandr Bondarevskyi <epickonst@gmail.com>
2022-01-13 17:59:20 +0100

    Replaced loop with ListView and ArrayAdapter, full remake of activity_numbers.xml

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,7 +17,9 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -42,15 +44,11 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-        for (int index = 0; index < words.size(); index++) {
+        ListView listView = (ListView) findViewById(R.id.list);
 
-            TextView wordView = new TextView(this);
-
-            wordView.setText(words.get(index));
-
-            rootView.addView(wordView);
-        }
+        listView.setAdapter(itemsAdapter);
     }
 }
